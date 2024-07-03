ee0905660919a6a2602226b1dbb8f357eade77d5
Faith Cyril <cyrilfaith17@gmail.com>
2024-07-03 10:58:09 +0000

    Implemented exception handling in serrvices

--- department-Service/src/main/java/com/javaguides/department_Service/service/DepartmentServiceImpl/DepartmentServiceImpl.java
+++ department-Service/src/main/java/com/javaguides/department_Service/service/DepartmentServiceImpl/DepartmentServiceImpl.java
@@ -2,6 +2,7 @@ package com.javaguides.department_Service.service.DepartmentServiceImpl;
 
 import com.javaguides.department_Service.dto.DepartmentDto;
 import com.javaguides.department_Service.entity.Department;
+import com.javaguides.department_Service.exception.ResourceNotFoundException;
 import com.javaguides.department_Service.mapper.DepartmentMapper;
 import com.javaguides.department_Service.repository.DepartmentRepository;
 import com.javaguides.department_Service.service.DepartmentService;
@@ -15,18 +16,21 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
-
+        try{
         Department department = DepartmentMapper.MAPPER.mapToDepartment(departmentDto);
         Department savedDepartment = departmentRepository.save(department);
-
         DepartmentDto savedDepartmentDto = DepartmentMapper.MAPPER.mapToDepartmentDto(savedDepartment);
         return savedDepartmentDto;
+        }
+        catch(Exception exception){
+            throw new RuntimeException("An error was encountered when saving department");
+        }
     }
 
     @Override
     public DepartmentDto getDepartmentByCode(String departmentCode) {
-        Department department = departmentRepository.findByDepartmentCode(departmentCode);
+        Department department = departmentRepository.findByDepartmentCode(departmentCode).orElseThrow(() -> new ResourceNotFoundException("Department","departmentCode",departmentCode));
         DepartmentDto departmentDto = DepartmentMapper.MAPPER.mapToDepartmentDto(department);
         return departmentDto;
-    }
+        }
 }
--- employee-Service/src/main/java/com/javaguides/employee_Service/service/EmployeeServiceImpl/EmployeeServiceImpl.java
+++ employee-Service/src/main/java/com/javaguides/employee_Service/service/EmployeeServiceImpl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.javaguides.employee_Service.service.EmployeeServiceImpl;
 
 import com.javaguides.employee_Service.employeedto.EmployeeDto;
 import com.javaguides.employee_Service.entity.Employee;
+import com.javaguides.employee_Service.exception.ResourceNotFoundException;
 import com.javaguides.employee_Service.mapper.EmployeeMapper;
 import com.javaguides.employee_Service.repository.EmployeeRepository;
 import com.javaguides.employee_Service.service.EmployeeService;
@@ -15,17 +16,19 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto save(EmployeeDto employeeDto) {
+        try{
         Employee employee = EmployeeMapper.MAPPER.mapToEmployee(employeeDto);
-
         Employee savedEmployee = employeeRepository.save(employee);
-
         EmployeeDto savedEmployeeDto = EmployeeMapper.MAPPER.mapToEmployeeDto(savedEmployee);
-        return savedEmployeeDto;
+        return savedEmployeeDto;}
+        catch(Exception exception){
+            throw new RuntimeException("Problem occurred while saving employee data");
+        }
     }
 
     @Override
     public EmployeeDto getEmployeeById(Long id) {
-        Employee employee = employeeRepository.findById(id).get();
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("employee","id",id));
         EmployeeDto employeeDto = EmployeeMapper.MAPPER.mapToEmployeeDto(employee);
         return employeeDto;
     }
